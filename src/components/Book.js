07b12743332ba8743ffa38134a842d0d85485983
Julz <jujasen@hotmail.com>
2021-04-24 14:04:48 +0200

    added yup to bookForm, datepicker not working

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,6 +3,27 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import DatePicker from "react-datepicker";
 import Button from './Button';
+import { Formik, useFormikContext } from 'formik'
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const validationSchema = yup.object().shape({
+    fromDate: yup.date()
+        .required("From date is required"),
+    toDate: yup.date()
+        .required("To date is required"),
+    firstName: yup.string()
+        .required("*First name is required"),
+    lastName: yup.string()
+        .required("*Last name is required"),
+    email: yup.string()
+        .email("*Email is invalid")
+        .required("*Email is required"),
+    phoneNumber: yup.number()
+        .integer("*Phone number is invalid")
+        .required("*Phone number is required")
+});
 
 
 const Book = ({ onClick }) => {
@@ -15,8 +36,30 @@ const Book = ({ onClick }) => {
         setRoomSelected(type);
     }
 
-    let today = new Date().toISOString().substr(0, 10);
+    const { book, handleSubmit } = useForm({
+        resolver: yupResolver(validationSchema),
+    });
 
+
+    const onSubmit = async (d) => {
+
+        // setSubmitting(true);
+        // setLoginError(null);
+
+        console.log(d);
+        
+
+        // try {
+        //     const response = await axios.post(`${BASE_URL}${AUTH_PATH}`, data);
+        //     console.log('response', response.data);
+        //     setAuth(response.data);
+        // } catch (error) {
+        //     console.log('error', error);
+        //     setLoginError(error.toString());
+        // } finally {
+        //     setSubmitting(false);
+        // }
+    };
 
     return (
         <>
@@ -83,102 +126,142 @@ const Book = ({ onClick }) => {
                         </div>
                     </div>
                 </div>
-                <form className="form">
-                    <fieldset className="form__fieldset">
-                        <div className="form__item">
-                            <p
-                            className="form__label"
-                            >* From date</p>
-                            <DatePicker
-                                selectsStart
-                                placeholderText="Select Start Date"
-                                selected={startDate}
-                                startDate={startDate}
-                                onChange={date => setStartDate(date)}
-                                minDate={new Date()} 
-                            />
-                            <p className="form__error">Error</p>
-                        </div>
-                        <div className="form__item">
-                            <p
-                            className="form__label"
-                            >* To date</p>
-                            <DatePicker
-                                selectsEnd
-                                placeholderText="Select Start Date"
-                                selected={endDate}
-                                endDate={endDate}
-                                onChange={date => setEndDate(date)}
-                                minDate={startDate} 
-                            />
-                            <p className="form__error">Error</p>
-                        </div>
-                        <div className="form__item">
-                            <p
-                                className="form__label"
-                            >* First name</p>
-                            <input
-                                id="firstName"
-                                className="form__input"
-                                placeholder="Type your first name"
-                                type="text"
-                            />
-                            <p className="form__error">Error</p>
-                        </div>
-                        <div className="form__item">
-                            <p
-                                className="form__label"
-                            >* Last name</p>
-                            <input
-                                id="lastName"
-                                className="form__input"
-                                placeholder="Type your last name"
-                                type="text"
-                            />
-                            <p className="form__error">Error</p>
-                        </div>
-                        <div className="form__item">
-                            <p
-                                className="form__label"
-                            >* E-mail</p>
-                            <input
-                                id="email"
-                                className="form__input"
-                                placeholder="Type your email adress"
-                                type="text"
-                            />
-                            <p className="form__error">Error</p>
-                        </div>
-                        <div className="form__item">
-                            <p
-                                className="form__label"
-                            >* Phone number</p>
-                            <input
-                                id="phoneNumber"
-                                className="form__input"
-                                placeholder="Type your phone number"
-                                type="tel"
-                            />
-                            <p className="form__error">Error</p>
-                        </div>
-                        <div className="form__item">
-                            <p
-                                className="form__label"
-                            >Special request</p>
-                            <textarea
-                                id="request"
-                                className="form__input"
-                                placeholder="Describe your request"
-                                type="text"
-                                rows="3">
-                            </textarea>
-                        </div>
-                        <div className="flex flex--center">
-                            <Button text="Book room"></Button>
-                        </div>
-                        
-                    </fieldset>
-                </form>
+                <Formik initialValues={{ fromDate: {startDate}, toDate: {endDate},firstName: "", lastName: "", email: "", phoneNumber: "" }} validationSchema={validationSchema}>
+                    {({ values,
+                        errors,
+                        handleChange,
+                        setFieldValue,
+                    handleBlur }) => ( 
+                        <form onSubmit={handleSubmit(onSubmit)} className="form">
+                            {console.log(values)}
+                            <fieldset className="form__fieldset">
+                            <div className="form__item">
+                                <p
+                                    className="form__label"
+                                >* From date</p>
+                                <DatePicker
+                                    id="fromDate"
+                                    ref={book}
+                                    selectsStart
+                                    placeholderText="Select Start Date"
+                                    selected={startDate}
+                                    startDate={startDate}
+                                    minDate={new Date()}
+                                    value={values.fromDate}
+                                    onChange={(date) => {
+                                        setStartDate(date);
+                                        setFieldValue(date);
+                                    }}
+                                />
+                                <p className="form__error">{errors.fromDate}</p>
+
+                            </div>
+                            <div className="form__item">
+                                <p
+                                    className="form__label"
+                                >* To date</p>
+                                <DatePicker
+                                    id="toDate"
+                                    ref={book}
+                                    selectsEnd
+                                    placeholderText="Select Start Date"
+                                    selected={endDate}
+                                    endDate={endDate}
+                                    minDate={startDate}
+                                    value={values.toDate}
+                                    onChange={(date) => {
+                                        setEndDate(date);
+                                        setFieldValue(date);
+                                    }}
+                                />
+                                <p className="form__error">{errors.toDate}</p>
+                            </div>
+                            <div className="form__item">
+                                <p
+                                    className="form__label"
+                                >* First name</p>
+                                <input
+                                    id="firstName"
+                                    ref={book}
+                                    className="form__input"
+                                    placeholder="Type your first name"
+                                    type="text"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.firstName}
+                                />
+                                <p className="form__error">{errors.firstName}</p>
+                            </div>
+                            <div className="form__item">
+                                <p
+                                    className="form__label"
+                                >* Last name</p>
+                                <input
+                                    id="lastName"
+                                    ref={book}
+                                    className="form__input"
+                                    placeholder="Type your last name"
+                                    type="text"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.lastName}
+                                />
+                                <p className="form__error">{errors.lastName}</p>
+                            </div>
+                            <div className="form__item">
+                                <p
+                                    className="form__label"
+                                >* E-mail</p>
+                                <input
+                                    id="email"
+                                    ref={book}
+                                    className="form__input"
+                                    placeholder="Type your email address"
+                                    type="text"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.email}
+                                />
+                                <p className="form__error">{errors.email}</p>
+                            </div>
+                            <div className="form__item">
+                                <p
+                                    className="form__label"
+                                >* Phone number</p>
+                                <input
+                                    id="phoneNumber"
+                                    ref={book}
+                                    className="form__input"
+                                    placeholder="Type your phone number"
+                                    type="number"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.phoneNumber}
+                                />
+                                <p className="form__error">{errors.phoneNumber}</p>
+                            </div>
+                            <div className="form__item">
+                                <p
+                                    className="form__label"
+                                >Special request</p>
+                                <textarea
+                                    id="request"
+                                    className="form__input"
+                                    placeholder="Describe your request"
+                                    type="text"
+                                    rows="3">
+                                </textarea>
+                            </div>
+                            <div className="flex flex--center">
+                                <Button type="submit" text="Book room"></Button>
+                            </div>
+                            </fieldset>
+                        </form>
+                )}
+                </Formik>
+                
+
+
 
             </div>
         </>
