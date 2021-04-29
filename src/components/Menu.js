9798f30178ace21cbd3ec6a70e7890ba966f7871
Julz <jujasen@hotmail.com>
2021-04-30 00:03:01 +0200

    added logout buttons to nav

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,23 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useState, useContext } from 'react';
 import logo from '../img/logo-white.png';
 import { HiOutlineMenuAlt1 } from 'react-icons/hi';
 import { CgClose } from 'react-icons/cg';
 import { FaTripadvisor } from 'react-icons/fa';
 import { AiOutlineFacebook, AiOutlineInstagram } from 'react-icons/ai';
+import AuthContext from '../utils/AuthContext';
 
 const Menu = () => {
 
     const [menuClosed, setMenu] = useState("false");
+    const [auth, setAuth] = useContext(AuthContext);
 
+    const history = useHistory();
+
+    function logout() {
+        setAuth(null);
+        history.push('/');
+    }
 
     const handleToggle = () => {
         setMenu(!menuClosed);
@@ -22,17 +30,21 @@ const Menu = () => {
                     <Link to="/">
                         <img className="menu__logo" src={logo} alt="Holidaze"></img>
                     </Link>
-                    {
-                        menuClosed ?
-                        <HiOutlineMenuAlt1
-                            onClick={handleToggle} className="menu__toggle">
-                        </HiOutlineMenuAlt1>
-                        :
-                        <CgClose
-                            onClick={handleToggle}
-                            className="menu__toggle">
-                        </CgClose>
-                    }
+                    <div className="flex flex--start">
+                        {auth ? <button className="button--simple" onClick={logout}>Log out</button> : ''}
+                        {
+                            menuClosed ?
+                                <HiOutlineMenuAlt1
+                                    onClick={handleToggle} className="menu__toggle">
+                                </HiOutlineMenuAlt1>
+                                :
+                                <CgClose
+                                    onClick={handleToggle}
+                                    className="menu__toggle">
+                                </CgClose>
+                        }
+                    </div>
+
                 </div>
                 { !menuClosed ? 
                     <div className="menu__open">
@@ -46,9 +58,16 @@ const Menu = () => {
                             <Link onClick={handleToggle} className="menu__link" to='/contact'>
                             Contact us
                         </Link>
+                            {auth ? <div className="menu__link" onClick={() => {
+                                logout();
+                                handleToggle();
+                            }}>Log out</div> 
+                            :
                             <Link onClick={handleToggle} className="menu__link" to='/login'>
-                            Log in
-                        </Link>
+                                    Log in
+                            </Link>
+                            }
+                        
                     </div>
                     <p>Follow us</p>
                     <div className="menu__socials flex flex--center">
